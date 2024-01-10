2d016fda7cdd278463074b674988db56ec9436b8
Federico Ceteroni <f.ceteroni.ext@teamsystem.com>
2024-01-10 17:27:06 +0100

    Aggiungere i file di progetto.

---
+++ BlazorFoto/Models/Foto.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorFoto.Models
+{
+    public class Foto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsVisible { get; set; }
+        public byte[]? ImageFile { get; set; }
+        public string ImagePath => ImagePath is null ? "https://cdn.dribbble.com/users/476251/screenshots/2619255/attachments/523315/placeholder.png?resize=400x300&vertical=center"
+                                                                : $"data:image/jpeg;base64, {Convert.ToBase64String(ImageFile)}";
+        public Foto() { }
+
+        public Foto(string name, string description, bool isVisible, byte[]? imageFile)
+        {
+            Name = name;
+            Description = description;
+            IsVisible = isVisible;
+            ImageFile = imageFile;
+        }
+    }
+}
